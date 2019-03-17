c25283a55fa9308d57f8d160893577fde1513f14
asaranprasad <ambikapathy.s@husky.neu.edu>
2019-03-17 18:58:31 -0400

    generated required components using ng g c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CourseGridComponent } from './components/course-grid/course-grid.component';
+import { ModuleListComponent } from './components/module-list/module-list.component';
+import { LessonTabsComponent } from './components/lesson-tabs/lesson-tabs.component';
+import { TopicPillsComponent } from './components/topic-pills/topic-pills.component';
+import { WidgetListComponent } from './components/widget-list/widget-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CourseGridComponent, ModuleListComponent, LessonTabsComponent, TopicPillsComponent, WidgetListComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
