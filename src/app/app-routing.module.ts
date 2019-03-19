7c4446e4891f2acdb4169faebf57fd2dd56fd3bf
asaranprasad <ambikapathy.s@husky.neu.edu>
2019-03-19 16:24:56 -0400

    trying out nested router pattern

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CourseComponent } from "./components/course/course.component";
+import { CourseGridComponent } from "./components/course-grid/course-grid.component";
+import { ModuleListComponent } from "./components/module-list/module-list.component";
+import { LessonTabsComponent } from "./components/lesson-tabs/lesson-tabs.component";
+import { WidgetListComponent } from "./components/widget-list/widget-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "course", pathMatch: "full" },
+  {
+    path: "course",
+    component: CourseGridComponent,
+    children: [
+      {
+        path: ":courseId",
+        component: CourseComponent,
+        children: [
+          {
+            path: "module",
+            component: ModuleListComponent,
+            children: [
+              {
+                path: ":moduleId/lesson",
+                component: LessonTabsComponent,
+                children: [
+                  {
+                    path: ":lessonId/widget",
+                    component: WidgetListComponent
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { CourseGridComponent } from './components/course-grid/course-grid.component';
-import { ModuleListComponent } from './components/module-list/module-list.component';
-import { LessonTabsComponent } from './components/lesson-tabs/lesson-tabs.component';
-import { TopicPillsComponent } from './components/topic-pills/topic-pills.component';
-import { WidgetListComponent } from './components/widget-list/widget-list.component';
+import { CourseGridComponent } from "./components/course-grid/course-grid.component";
+import { ModuleListComponent } from "./components/module-list/module-list.component";
+import { LessonTabsComponent } from "./components/lesson-tabs/lesson-tabs.component";
+import { TopicPillsComponent } from "./components/topic-pills/topic-pills.component";
+import { WidgetListComponent } from "./components/widget-list/widget-list.component";
+import { CourseComponent } from './components/course/course.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseGridComponent, ModuleListComponent, LessonTabsComponent, TopicPillsComponent, WidgetListComponent],
+  declarations: [
+    AppComponent,
+    CourseGridComponent,
+    ModuleListComponent,
+    LessonTabsComponent,
+    TopicPillsComponent,
+    WidgetListComponent,
+    CourseComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
