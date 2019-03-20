ebc57fe2dac464fda34b00e2fb1dfc826fee5d47
asaranprasad <ambikapathy.s@husky.neu.edu>
2019-03-20 13:16:00 -0400

    implemented module list component

--- src/app/components/module-list/module-list.component.ts
+++ src/app/components/module-list/module-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { ModuleService } from "src/app/services/module.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-module-list',
-  templateUrl: './module-list.component.html',
-  styleUrls: ['./module-list.component.css']
+  selector: "app-module-list",
+  templateUrl: "./module-list.component.html",
+  styleUrls: ["./module-list.component.css"]
 })
 export class ModuleListComponent implements OnInit {
+  @Input() courseId;
+  modules = [];
+  selectedModule = {};
+  constructor(private route: ActivatedRoute, private service: ModuleService) {}
 
-  constructor() { }
+  selectModule = module => {
+    this.selectedModule = module;
+  };
 
   ngOnInit() {
+    this.service
+      .findModulesForCourse(this.route.snapshot.paramMap.get("courseId"))
+      .then(modules => {
+        this.modules = modules;
+        console.log(modules);
+      });
   }
-
 }
